496b10787226deb77015fe2243bcea59d086008f
iiDkRemastered <iiDkRemastered@noreply.github.com>
2026-09-24 00:08:49 +0100

    release

---
+++ NameTag.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace NameTags
+{
+    internal class NameTag : MonoBehaviour
+    {
+        public UnityEngine.UI.Text text;
+        public VRRig rig;
+
+        void LateUpdate()
+        {
+            if (rig.enabled)
+            {
+                transform.LookAt(GorillaTagger.Instance.headCollider.transform.position);
+                text.text = rig.Creator.NickName;
+                text.color = rig.mainSkin.material.name.Contains("fected") ? new Color(1f, 0.5f, 0f, 1) : rig.playerColor;
+            } else
+                text.text = "null";
+        }
+
+        // The-Graze/WhoIsTalking
+        private Color ColourHandling()
+        {
+            if (rig.bodyRenderer.cosmeticBodyType == GorillaBodyType.Skeleton)
+                return Color.green;
+            
+            switch (rig.setMatIndex)
+            {
+                case 1:
+                    return Color.red;
+                case 2:
+                case 11:
+                    return new Color32(255, 128, 0, 255);
+                case 3:
+                case 7:
+                    return Color.blue;
+                case 12:
+                    return Color.green;
+                default:
+                    return rig.playerColor;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace NameTags
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private List<VRRig> ntGiven = new List<VRRig> { };
+        void Update()
+        {
+            if (GorillaLocomotion.GTPlayer.Instance == null)
+                return;
+
+            foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+            {
+                if (vrrig != GorillaTagger.Instance.offlineVRRig && !ntGiven.Contains(vrrig))
+                {
+                    ntGiven.Add(vrrig);
+
+                    GameObject NameTagObject = LoadAsset("NameTag");
+                    NameTagObject.transform.SetParent(vrrig.headMesh.transform, false);
+
+                    GameObject Canvas = NameTagObject.transform.Find("Canvas").gameObject;
+
+                    NameTag a = Canvas.AddComponent<NameTag>();
+                    a.text = Canvas.transform.Find("Name").GetComponent<UnityEngine.UI.Text>();
+                    a.rig = vrrig;
+                }
+            }
+        }
+
+        static AssetBundle assetBundle;
+        public static GameObject LoadAsset(string assetName)
+        {
+            GameObject gameObject = null;
+
+            if (assetBundle == null)
+            {
+                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("NameTags.Resources.nametag");
+                assetBundle = AssetBundle.LoadFromStream(stream);
+                stream.Close();
+            }
+            gameObject = Instantiate(assetBundle.LoadAsset<GameObject>(assetName));
+
+            return gameObject;
+        }
+    }
+}
